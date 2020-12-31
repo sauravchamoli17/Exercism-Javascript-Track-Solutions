b01b0589f47634ec441d0eb9b102e6b7a87c4015
Saurav Chamoli <sauravchamoli17@gmail.com>
2020-12-31 19:09:57 +0530

    Nucleotide Updated!

--- nucleotide-count/nucleotide-count.js
+++ nucleotide-count/nucleotide-count.js
@@ -1,8 +1,7 @@
 export class NucleotideCounts {
   static parse(strand) {
     const count = {A: 0, C: 0, G: 0, T: 0};
-    const invalid = strand.match(/[^ACGT]/g);
-    if(invalid != null && invalid.length > 0)
+    if(/[^ACGT]/g.test(strand))
      throw new Error('Invalid nucleotide in strand');
     Object.keys(count).forEach(key => count[key] = strand.split('').filter(char => char === key).length);
     return `${count.A} ${count.C} ${count.G} ${count.T}`;
