7f675d2503b8e3de567d085fc0ce8a09e9f0a090
Saurav Chamoli <sauravchamoli17@gmail.com>
2021-01-01 17:13:08 +0530

    Isogram Completed!

--- isogram/isogram.js
+++ isogram/isogram.js
@@ -1,4 +1,4 @@
 export const isIsogram = (word) => {
    word = word.replace(/[-\s]/g,'').toLowerCase();
-   return word.split('').every((char, index) => word.indexOf(char) ===      index);
+   return word.split('').every((char, index) => word.indexOf(char) === index);
 };
