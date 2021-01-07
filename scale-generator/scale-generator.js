fe982af87aca2b455853d547155f2cd5b25db9cd
Saurav Chamoli <sauravchamoli17@gmail.com>
2021-01-07 19:46:07 +0530

    Square Root

---
+++ scale-generator/scale-generator.js
@@ -0,0 +1,15 @@
+export class Scale {
+  constructor(tonic) {
+    
+  }
+
+  chromatic() {
+    
+  }
+
+  interval(intervals) {
+    
+  }
+}
+
+// throw new Error('Remove this statement and implement this function');
