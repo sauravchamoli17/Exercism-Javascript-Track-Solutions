1dd911c7d0e5949de17d88603cf4aa692fdbf785
Saurav Chamoli <sauravchamoli17@gmail.com>
2021-01-08 19:54:18 +0530

    Matrix

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,17 +1,17 @@
 // throw new Error('Remove this statement and implement this function');
 
-// class Matrix {
-//   constructor(matrixString) {
-//     this.row = matrixString.split('\n');
-//     console.log(this.row);
-//   }  
-//   get rows() {
-    
-//   }
+class Matrix {
+  constructor(matrixNotation) {
+    this.row = matrixNotation.split('\n');
+  }  
+  get rows() {
+    let matrixRow = this.row.map(element => element.split(' '));
+    console.log(typeof matrixRow);
+  }
   
-//   get columns() {
+  get columns() {
     
-//   }
-// }
+  }
+}
 
-// new Matrix('1 2\n10 20').rows;
+new Matrix('1 2\n10 20').row;
